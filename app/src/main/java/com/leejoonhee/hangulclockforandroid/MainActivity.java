a07408a410db65eb089fb8321fd92de52e24d2cf
LeeJoonHee <leejoonhee@LeeJoonHees-MacBook-Pro.local>
2018-01-31 20:21:41 +0900

    1.7version update

--- app/src/main/java/com/leejoonhee/hangulclockforandroid/MainActivity.java
+++ app/src/main/java/com/leejoonhee/hangulclockforandroid/MainActivity.java
@@ -1,16 +1,21 @@
 package com.leejoonhee.hangulclockforandroid;
 
+import android.net.Uri;
 import android.os.*;
 import android.support.v7.app.*;
 import android.view.*;
 import android.content.*;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -19,11 +24,19 @@ public class MainActivity extends AppCompatActivity
 	DatabaseReference version = rootRef.child("version");
 	TextView mversion;
 	TextView mdownloads;
+	private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+		// Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+		MobileAds.initialize(this, "ca-app-pub-8081631582008293~8449303451");
+
+		mAdView = findViewById(R.id.adView);
+		AdRequest adRequest = new AdRequest.Builder().build();
+		mAdView.loadAd(adRequest);
 
 		mversion = (TextView)findViewById(R.id.version);
 		mdownloads = (TextView)findViewById(R.id.download);
@@ -35,6 +48,23 @@ public class MainActivity extends AppCompatActivity
 	
 	public void help(View v){
 		startActivity(new Intent(this, HelpActivity.class));
+	}
+
+	public void pc(View v){
+		String url ="https://github.com/dsa28s/windows-hangul-clock";
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+		startActivity(intent);
+	}
+
+	public void android(View v){
+		String url ="https://github.com/neo81389/HangulClock";
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+		startActivity(intent);
+	}
+
+	public void update(View v){
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.leejoonhee.hangulclockforandroid"));
+		startActivity(intent);
 	}
 
 	@Override
@@ -60,11 +90,11 @@ public class MainActivity extends AppCompatActivity
 			public void onDataChange(DataSnapshot dataSnapshot) {
 				String text = dataSnapshot.getValue(String.class);
 
-				if(text.equals("1.4"))
+				if(text.equals("1.7"))
 					mversion.setText("현재 최신버전입니다");
 
 				else
-					mversion.setText("업데이트가 있습니다 <<업데이트하러가기>>");
+					mversion.setText("[업데이트하러가기]");
 			}
 
 			@Override
--- app/src/main/java/com/leejoonhee/hangulclockforandroid/SettingActivity.java
+++ app/src/main/java/com/leejoonhee/hangulclockforandroid/SettingActivity.java
@@ -8,23 +8,19 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.RemoteViews;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SettingActivity extends AppCompatActivity
 {
-	SharedPreferences sets;
-	EditText words;
-	CheckBox mode;
+    SharedPreferences sets;
+    EditText words;
 
-	ImageView image;
-	TextView sentence;
-	
+    ImageView image;
+    TextView sentence;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -33,50 +29,119 @@ public class SettingActivity extends AppCompatActivity
         image = (ImageView)findViewById(R.id.backgroud);
         sentence = (TextView)findViewById(R.id.title);
 
-		words = (EditText)findViewById(R.id.editText1); //표시할문자 값을 받아주기위해 선언하였습니다
-        mode = (CheckBox)findViewById(R.id.mode); //색상을 정하도록 문자값을 받습니다(문자로 받는것은 또 다른 기능을 구현하기 위해 일부러 문자로 받았습니다)
+        words = (EditText)findViewById(R.id.editText1); //표시할문자 값을 받아주기위해 선언하였습니다
 
         sets = getSharedPreferences("usersets", Activity.MODE_PRIVATE); //MainActivity가 꺼져도 NewAppWidget에서 값을 받아 올 수 있도록 SharedPreference를 사용하였습니다
         words.setText(sets.getString("title", "언젠가는 빛이 될 당신"));
 
-        if(sets.getInt("color", 0) == 2){
-            mode.setChecked(true);
-			image.setImageResource(R.drawable.hangulclock_board_black_prev);
-			sentence.setTextColor(Color.BLACK);
-        }
         if (sets.getInt("color", 0) == 1){
-            mode.setChecked(false);
-			image.setImageResource(R.drawable.hangulclock_board_white_prev);
-			sentence.setTextColor(Color.WHITE);
+            image.setImageResource(R.drawable.hangulclock_board_white_prev);
+            sentence.setTextColor(Color.WHITE);
+        }
+
+        if(sets.getInt("color", 0) == 2){
+            image.setImageResource(R.drawable.hangulclock_board_black_prev);
+            sentence.setTextColor(Color.BLACK);
+        }
+
+        if (sets.getInt("color", 0) == 3){
+            image.setImageResource(R.drawable.hangulclock_board_pink_prev);
+            sentence.setTextColor(getResources().getColor(R.color.pink));
+        }
+
+        if (sets.getInt("color", 0) == 4){
+            image.setImageResource(R.drawable.hangulclock_board_red_prev);
+            sentence.setTextColor(getResources().getColor(R.color.red));
+        }
+
+        if (sets.getInt("color", 0) == 5){
+            image.setImageResource(R.drawable.hangulclock_board_orange_prev);
+            sentence.setTextColor(getResources().getColor(R.color.orange));
+        }
+
+        if (sets.getInt("color", 0) == 6){
+            image.setImageResource(R.drawable.hangulclock_board_green_prev);
+            sentence.setTextColor(getResources().getColor(R.color.green));
+        }
+
+        if (sets.getInt("color", 0) == 7){
+            image.setImageResource(R.drawable.hangulclock_board_bluegreen_prev);
+            sentence.setTextColor(getResources().getColor(R.color.bluegreen));
         }
     }
 
-	public void save(View v){
-		SharedPreferences.Editor editor = sets.edit();
-		editor.putString("title", words.getText().toString()); //edittext에서 얻어온 값을 title로 저장합니다.
-		editor.commit();
+    public void save(View v){
+        SharedPreferences.Editor editor = sets.edit();
+        editor.putString("title", words.getText().toString()); //edittext에서 얻어온 값을 title로 저장합니다.
+        editor.commit();
 
-		sentence.setText(sets.getString("title", "receivingfail"));
+        sentence.setText(sets.getString("title", "receivingfail"));
 
-		if(mode.isChecked()){
-			editor.putInt("color", 2); //ID가"color"인 sharedpreference에 정수 "2"를 저장합니다
-			editor.commit();
-			image.setImageResource(R.drawable.hangulclock_board_black_prev);
-			sentence.setTextColor(Color.BLACK);
-		}
-		else{
-			editor.putInt("color", 1); //ID가"color"인 sharedpreference에 정수 "1"를 저장합니다
-			editor.commit();
-			image.setImageResource(R.drawable.hangulclock_board_white_prev);
-			sentence.setTextColor(Color.WHITE);
-		}
-		Toast.makeText(getApplicationContext(), "적용되었습니다", Toast.LENGTH_SHORT).show(); //저장 버튼 클릭시 적용됨을 알리기위해 메세지를 띄웁니다
-	}
+        Toast.makeText(getApplicationContext(), "적용되었습니다", Toast.LENGTH_SHORT).show(); //저장 버튼 클릭시 적용됨을 알리기위해 메세지를 띄웁니다
+    }
 
-	public void update(View v){
-		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.leejoonhee.hangulclockforandroid"));
-		startActivity(intent);
-	}
-	
+    public void setwhite(View v) {
+        SharedPreferences.Editor editor = sets.edit();
+        editor.putInt("color", 1); //ID가"color"인 sharedpreference에 정수 "1"를 저장합니다
+        editor.commit();
+        sentence.setTextColor(Color.WHITE);
+        image.setImageResource(R.drawable.hangulclock_board_white_prev);
+        Toast.makeText(getApplicationContext(), "선택되었습니다", Toast.LENGTH_SHORT).show(); //저장 버튼 클릭시 적용됨을 알리기위해 메세지를 띄웁니다
+    }
+
+    public void setblack(View v) {
+        SharedPreferences.Editor editor = sets.edit();
+        editor.putInt("color", 2); //ID가"color"인 sharedpreference에 정수 "1"를 저장합니다
+        editor.commit();
+        sentence.setTextColor(Color.BLACK);
+        image.setImageResource(R.drawable.hangulclock_board_black_prev);
+        Toast.makeText(getApplicationContext(), "선택되었습니다", Toast.LENGTH_SHORT).show(); //저장 버튼 클릭시 적용됨을 알리기위해 메세지를 띄웁니다
+    }
+
+    public void setpink(View v) {
+        SharedPreferences.Editor editor = sets.edit();
+        editor.putInt("color", 3); //ID가"color"인 sharedpreference에 정수 "1"를 저장합니다
+        editor.commit();
+        sentence.setTextColor(getResources().getColor(R.color.pink));
+        image.setImageResource(R.drawable.hangulclock_board_pink_prev);
+        Toast.makeText(getApplicationContext(), "선택되었습니다", Toast.LENGTH_SHORT).show(); //저장 버튼 클릭시 적용됨을 알리기위해 메세지를 띄웁니다
+    }
+
+    public void setred(View v) {
+        SharedPreferences.Editor editor = sets.edit();
+        editor.putInt("color", 4); //ID가"color"인 sharedpreference에 정수 "1"를 저장합니다
+        editor.commit();
+        sentence.setTextColor(getResources().getColor(R.color.red));
+        image.setImageResource(R.drawable.hangulclock_board_red_prev);
+        Toast.makeText(getApplicationContext(), "선택되었습니다", Toast.LENGTH_SHORT).show(); //저장 버튼 클릭시 적용됨을 알리기위해 메세지를 띄웁니다
+    }
+
+    public void setorange(View v) {
+        SharedPreferences.Editor editor = sets.edit();
+        editor.putInt("color", 5); //ID가"color"인 sharedpreference에 정수 "1"를 저장합니다
+        editor.commit();
+        sentence.setTextColor(getResources().getColor(R.color.orange));
+        image.setImageResource(R.drawable.hangulclock_board_orange_prev);
+        Toast.makeText(getApplicationContext(), "선택되었습니다", Toast.LENGTH_SHORT).show(); //저장 버튼 클릭시 적용됨을 알리기위해 메세지를 띄웁니다
+    }
+
+    public void setgreen(View v) {
+        SharedPreferences.Editor editor = sets.edit();
+        editor.putInt("color", 6); //ID가"color"인 sharedpreference에 정수 "1"를 저장합니다
+        editor.commit();
+        sentence.setTextColor(getResources().getColor(R.color.green));
+        image.setImageResource(R.drawable.hangulclock_board_green_prev);
+        Toast.makeText(getApplicationContext(), "선택되었습니다", Toast.LENGTH_SHORT).show(); //저장 버튼 클릭시 적용됨을 알리기위해 메세지를 띄웁니다
+    }
+
+    public void setbluegreen(View v) {
+        SharedPreferences.Editor editor = sets.edit();
+        editor.putInt("color", 7); //ID가"color"인 sharedpreference에 정수 "1"를 저장합니다
+        editor.commit();
+        sentence.setTextColor(getResources().getColor(R.color.bluegreen));
+        image.setImageResource(R.drawable.hangulclock_board_bluegreen_prev);
+        Toast.makeText(getApplicationContext(), "선택되었습니다", Toast.LENGTH_SHORT).show(); //저장 버튼 클릭시 적용됨을 알리기위해 메세지를 띄웁니다
+    }
+
 }
 
